bbd0e336e4aee7a0c1aad76bacaa9b187954a864
Sarolus <cactussius.primus@gmail.com>
2021-02-03 12:31:27 +0000

    4.Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * This is the puts function as asked in the task 4.Hello, puts,
+ * as asked in the project 0x00.C-Hello, World.
+ */
+
+int main(void)
+{
+	puts(\""Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
