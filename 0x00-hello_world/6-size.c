44936f74ade4d76594810bf9e8e917d4ccdaa0a3
Sarolus <cactussius.primus@gmail.com>
2021-02-03 14:08:45 +0000

    Fixing type symbol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %c\n", sizeof(char));
-	printf("Size of an int: %i\n", sizeof(int));
-	printf("Size of a long int: %li\n", sizeof(long int));
-	printf("Size of a long long int: %lli\n", sizeof(long long int));
-	printf("Size of a float: %f\n", sizeof(float));
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
 
 	return (0);
 }
