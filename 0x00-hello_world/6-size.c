77b5820b6dd867e96f6acc07bb6b183918ca8b14
Sarolus <cactussius.primus@gmail.com>
2021-02-03 14:00:36 +0000

    Size of various type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print size of various type
+ * Return: After printing values, return 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %c\n");
+	printf("Size of an int: %i\n");
+	printf("Size of a long int: %li\n");
+	printf("Size of a long long int: %lli\n");
+	printf("Size of a float: %f\n");
+
+	return (0);
+}
