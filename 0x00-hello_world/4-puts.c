8cedd5d986037bb00d8f49224df2cb5f615fdfa9
Sarolus <cactussius.primus@gmail.com>
2021-02-03 13:14:20 +0000

    Correcting useless \n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Return: After printing, return 0
  */
 
-void main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
