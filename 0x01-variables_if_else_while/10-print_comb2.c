40cd99d9883cfbedb029b68192f9b26b4a7134f0
Sarolus <cactussius.primus@gmail.com>
2021-02-04 17:51:57 +0100

    All the way

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print single-digit numbers
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int digit;
+	char space;
+
+	for (digit = 0; digit < 100 ; digit++)
+	{
+		putchar((digit / 10) + '0');
+		putchar((digit % 10) + '0');
+		if (digit != 99)
+		{
+				putchar(',');
+				putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
