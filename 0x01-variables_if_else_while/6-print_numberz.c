4f4f404765641274fe125b027fb3f6ecdcde5ac0
Sarolus <cactussius.primus@gmail.com>
2021-02-04 16:27:31 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digits of base 10
+ * Return: always return 0
+ */
+
+int main(void)
+{
+
+	int digit;
+
+	for (digit = 48; digit <= 57; digit++)
+	{
+		putchar(digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
