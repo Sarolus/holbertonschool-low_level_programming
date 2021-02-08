3d5ca4b4c951b44d761bca0a604ac6a045e50a8a
Sarolus <cactussius.primus@gmail.com>
2021-02-08 14:33:45 +0100

    holberton.h and _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - Write the specified character
+ * @c: The character to print
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Here are my works about the project 0x02.C-Functions, nested loops, you should
+find the different tasks and all the files that were needed to complete the
+project.
