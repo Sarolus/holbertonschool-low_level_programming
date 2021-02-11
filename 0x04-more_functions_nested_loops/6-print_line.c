caeebae1d2c4e894df6856f10e438a7ef4a8a4bb
Sarolus <cactussius.primus@gmail.com>
2021-02-11 14:58:08 +0100

    Line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,9 +2,20 @@
 
 /**
  * print_line - Draw a straight line in the terminal
+ * @n: number of times the character is printed
  */
 
-void print_line(int n);
+void print_line(int n)
 {
-	int n;
+	int loop;
+
+	if (n > 0)
+	{
+		for (loop = 0; loop < n ; loop++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
 }
