fc171c497889c42697e586241dd27c4b892c9a74
Sarolus <cactussius.primus@gmail.com>
2021-02-11 13:40:57 +0100

    Making betty happy

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,16 @@
 
 /**
  * print_numbers - Print numbers from 0 to 9
- * Return: return 0
  */
 
 void print_numbers(void)
 {
 	int nb;
 
-	for (nb = 0; nb <= 9; nb++)
+	for (nb = '0'; nb <= '9'; nb++)
 	{
 		_putchar(nb);
 	}
 
 	_putchar('\n');
-	return;
 }
