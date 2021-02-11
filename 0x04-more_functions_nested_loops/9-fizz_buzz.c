ba2cd7584688cf2b8a7675d6975cacaeef5013a4
Sarolus <cactussius.primus@gmail.com>
2021-02-11 17:43:53 +0100

    Fizzbuzzing

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Fizzbuzzing
+ * Return: return 0
+ */
+
+int main(int nb)
+{
+	for (nb = 1; nb <= 100; nb++)
+	{
+		if (nb % 3 == 0 && nb % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (nb % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (nb % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", nb);
+		}
+	}
+	return (0);
+}
