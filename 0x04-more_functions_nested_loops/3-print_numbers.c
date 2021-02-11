cfda63312e9325b8747e7d368fca55f957294ff5
Sarolus <cactussius.primus@gmail.com>
2021-02-11 13:34:14 +0100

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ * Return: return 0
+ */
+
+void print_numbers(void);
+{
+	int nb;
+
+	for (nb = 0; nb <= 9; nb++)
+	{
+		_putchar(nb);
+	}
+
+	_putchar('\n');
+	return (0);
+}
