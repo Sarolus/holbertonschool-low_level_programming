3d8b999d6e3e7e080215be0c6b594c44d6dbf7cb
Sarolus <cactussius.primus@gmail.com>
2021-02-17 16:08:41 +0100

    Convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Convert a string to an integer
+ * @str: string
+ * Return: return the result
+ */
+
+int _atoi(char *str)
+{
+	int result = 0;
+	int symbol = 1;
+	int length;
+
+	if (str[0] == '-')
+	{
+		symbol *= -1;
+	}
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		result = ((result * 10) + (str[length] - '0'));
+	}
+
+	return (symbol * result);
+}
