59c52dc1abf88bfe38ec7121f23a699e66ed0561
Sarolus <cactussius.primus@gmail.com>
2021-02-19 15:22:50 +0100

    Capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @src: source string
+ * Return: return length
+ */
+
+int _strlen(char *src)
+{
+	int length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * _isalpha - checks alphabetic character
+ * @c: character
+ * Return: return 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: string
+ * Return: return the changed string
+ */
+
+char *cap_string(char *str)
+{
+	int index;
+	int length = _strlen(str);
+
+	for (index = 1; index < length; index++)
+	{
+		if (_isalpha(str[index]))
+		{
+			if (str[index - 1] >= 0 && str[index - 1] <= 47)
+			{
+				str[index] -= 32;
+			}
+		}
+	}
+
+	return (str);
+}
