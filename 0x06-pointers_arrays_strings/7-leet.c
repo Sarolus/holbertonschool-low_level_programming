f4207ea90e06c78031bef2baa82d798aca291905
Sarolus <cactussius.primus@gmail.com>
2021-02-19 18:33:21 +0100

    l33ter4lly sp34k1ng

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+
+char *leet(char *str)
+{
+	int index1, index2;
+	char *letters = "aAeEoOtTlL";
+	char *l33t = "4433007711";
+
+	for (index1 = 0; str[index1] != '\0'; index1++)
+	{
+		for (index2 = 0; letters[index2] != '\0'; index2++)
+		{
+			if (str[index1] == letters[index2])
+			{
+				str[index1] = l33t[index2];
+			}
+		}
+	}
+
+	return (str);
+}
