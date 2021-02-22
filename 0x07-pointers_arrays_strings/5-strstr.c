594b4f9db942f040526c1f8967c0b914fea527b6
Sarolus <cactussius.primus@gmail.com>
2021-02-22 19:31:45 +0100

    Fix strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,28 @@
 #include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _strcmp - Compare two strings
+ * @s1 : source string
+ * @s2 : source string
+ * Return: return 1 if different, 0 if similar
+ */
+
+int _strcmp(char *haystack, char *needle)
+{
+	while (*haystack && *needle)
+	{
+		if (*haystack != *needle)
+		{
+			return (0);
+		}
+
+		haystack++;
+		needle++;
+	}
+
+	return (*needle == '\0');
+}
 
 /**
  *
@@ -7,5 +31,14 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	
+	while (*haystack != '\0')
+	{
+		if ((*haystack == *needle) && _strcmp(haystack, needle))
+		{
+			return (haystack);
+			haystack++;
+		}
+	}
+
+	return (NULL);
 }
