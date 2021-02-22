2c14dc1aa00adda050555807b4046a0b8373b287
Sarolus <cactussius.primus@gmail.com>
2021-02-22 20:27:46 +0100

    Diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix
+ * @a: array
+ * @size: size of square
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
