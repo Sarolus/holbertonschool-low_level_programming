a3bf88619058ee50a39b9bef291b87fdd6ec54d3
Sarolus <cactussius.primus@gmail.com>
2021-02-22 10:53:35 +0100

    Locate character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String
+ * @c: Character
+ * Return: Return c or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
