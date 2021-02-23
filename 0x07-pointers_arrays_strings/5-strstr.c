8442ad0c13976a38a74cd29e6c189371901e6c71
Sarolus <cactussius.primus@gmail.com>
2021-02-23 21:19:52 +0100

    Fix loops

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,6 +12,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	i = 0;
+
+	if (needle[i] == '\0')
+	{
+		return (haystack + i);
+	}
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
