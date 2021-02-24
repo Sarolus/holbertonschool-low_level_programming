e351493e4d5ff746188e01a626cfad6a119e5965
Sarolus <cactussius.primus@gmail.com>
2021-02-24 14:49:31 +0100

    Fix betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 
 /**
- *
- *
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x: Base number
+ * @y: Power number
+ * Return: return -1 if less than 0, 1 if 0 otherwise the value
  */
 
 int _pow_recursion(int x, int y)
