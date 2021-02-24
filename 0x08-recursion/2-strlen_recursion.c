dc0a176119fdd1d8258aae0e659ed3172e70c7f6
Sarolus <cactussius.primus@gmail.com>
2021-02-24 13:53:28 +0100

    Length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: String
+ * Return: length of the string otherwise 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+	return (0);
+}
