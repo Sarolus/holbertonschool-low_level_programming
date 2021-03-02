82dcdab25f3ca297ef3dd1a3bf3113142696d09a
Sarolus <cactussius.primus@gmail.com>
2021-03-02 15:42:00 +0100

    Fix conditions

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,25 +3,6 @@
 #include <stddef.h>
 
 /**
- * _strlen - Returns lenght of a string.
- * @s: string
- * Return: Return result
- */
-
-int _strlen(char *s)
-{
-	int length;
-
-	length = 0;
-	while (s && s[length] != '\0')
-	{
-		length++;
-	}
-
-	return (length);
-}
-
-/**
  * str_concat - Concatenate two strings.
  * @s1: First string
  * @s2: Second string
@@ -30,16 +11,24 @@ int _strlen(char *s)
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2;
+	int i;
+	int len1 = 0;
+	int len2 = 0;
 	char *dup;
 
 	if (s1 != NULL)
 	{
-		len1 = _strlen(s1);
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
 	}
 	if (s2 != NULL)
 	{
-		len2 = _strlen(s2);
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
 	}
 
 	dup = malloc((len1 + len2 + 1) * sizeof(char));
