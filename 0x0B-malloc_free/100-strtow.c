de5dcc20ae1301c9d193f89a3e1390e395d39db7
Sarolus <cactussius.primus@gmail.com>
2021-03-02 19:25:10 +0100

    Prototype

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: targeted string
+ * Return: return
+ */
+
+char **strtow(char *str)
+{
+	char **words;
+
+	words = '\0';
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	return (words);
+}
