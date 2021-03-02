aac883badc5baf781d6d01f9f460636f428a8b77
Sarolus <cactussius.primus@gmail.com>
2021-03-02 13:41:32 +0100

    Duplicate with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strlen - Returns lenght of a string.
+ * @s: string
+ * Return: Return result
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	;
+
+	return (length);
+}
+
+/**
+ * _strdup - Return a pointet to a newly allocated space in memory
+ * @str: Source string
+ * Return: return str to a new string dup, NULL if str = NULL, and if dup = NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i;
+
+	dup = malloc((_strlen(str) + 1) * sizeof(char));
+
+	if (str = NULL)
+	{
+		return (NULL);
+	}
+
+	if (dup = NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	return (dup)
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -62,3 +62,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s, char *s2);
