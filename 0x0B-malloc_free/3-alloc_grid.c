b9622d194c884fa999ed81f636c06f4b2778e7b7
Sarolus <cactussius.primus@gmail.com>
2021-03-02 18:02:34 +0100

    Fix free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid, lin, col;
+	int **grid, li, col;
 
 	if (width <= 0 || height <= 0)
 	{
@@ -23,22 +23,23 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (lin = 0; lin < height; lin++)
+	for (li = 0; li < height; li++)
 	{
-		grid[lin] = malloc(width * sizeof(int));
+		grid[li] = malloc(width * sizeof(int));
 
-		if (grid[lin] == NULL)
+		if (grid[li] == NULL)
 		{
+			free(grid[line]);
+			free(grid);
 			return (NULL);
 		}
-		for (col = 0; col < width; col++)
-		{
-			grid[col] = malloc(height * sizeof(int));
+	}
 
-			if (grid[col] == NULL)
-			{
-				return (NULL);
-			}
+	for (col = 0; col < width; col++)
+	{
+		for (li = 0; li < heigth; li++)
+		{
+			grid[li][col] = 0;
 		}
 	}
 
