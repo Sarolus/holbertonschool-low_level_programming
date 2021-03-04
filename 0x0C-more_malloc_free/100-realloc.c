008a301df3871d0d0697fabd83363e5012cef6c6
Sarolus <cactussius.primus@gmail.com>
2021-03-04 17:11:45 +0100

    Realloc prototype

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * realloc - reallocates a meory block
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size, in bytes, of the allocated space for ptr
+ * @new_size: new size, in bytes of the new memory block
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	ptr = 0;
+	
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+}
