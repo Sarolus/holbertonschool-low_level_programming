555d8f6323409baba464269004a4f89a4cf39cef
Sarolus <cactussius.primus@gmail.com>
2021-03-04 15:32:25 +0100

    Betty fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,49 +18,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *dup;
 
 	if (s1 != NULL)
-	{
 		while (s1[len1] != '\0')
-		{
 			len1++;
-		}
-	}
+
 	else
-	{
 		s1 = "";
-	}
 
 	if (s2 != NULL)
-	{
 		while (s2[len2] != '\0')
-		{
 			len2++;
-		}
-	}
+
 	else
-	{
 		s2 = "";
-	}
 
 	if (n >= len2)
-	{
 		n = len2;
-	}
 
 	dup = malloc((len1 + n + 1) * sizeof(char));
 
 	if (!dup)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		dup[i] = s1[i];
-	}
+
 	for (i = 0; i < n; i++)
-	{
 		dup[i + len1] = s2[i];
-	}
+
 	dup[len1 + n] = '\0';
 
 	return (dup);
