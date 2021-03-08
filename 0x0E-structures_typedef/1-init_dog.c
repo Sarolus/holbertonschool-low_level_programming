5d6128e642718e61f9a862e39a336868ccd42294
Sarolus <cactussius.primus@gmail.com>
2021-03-08 17:50:49 +0100

    Fix doggo

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,18 +3,19 @@
 #include <stddef.h>
 
 /**
- *
- *
+ * init_dog - Initialize a variable of type struct dog
+ * @d: structure pointer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
 }
