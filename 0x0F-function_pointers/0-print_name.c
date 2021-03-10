f9d18bb07a23a2cdc3871a60fb2dc74c619095cb
Sarolus <cactussius.primus@gmail.com>
2021-03-10 14:37:21 +0100

    FIX HEADER

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stddef.h>
 
 /**
  * print_name - Prints a name
@@ -9,10 +8,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL && f == NULL)
+	if (name && f)
 	{
-		return;
+		(*f)(name);
 	}
-
-	(*f)(name);
 }
