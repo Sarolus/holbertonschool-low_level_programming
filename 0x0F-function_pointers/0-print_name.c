235c5a0ebba934ac9b29ef340423941f41548e2b
Sarolus <cactussius.primus@gmail.com>
2021-03-10 14:35:46 +0100

    Fix header

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - Prints a name
@@ -8,8 +9,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name == NULL && f == NULL)
 	{
-		(*f)(name);
+		return;
 	}
+
+	(*f)(name);
 }
