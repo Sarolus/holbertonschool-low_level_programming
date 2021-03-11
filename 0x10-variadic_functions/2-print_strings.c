e7c74e88dc584aba16c0a50c73d469ceb0609941
Sarolus <cactussius.primus@gmail.com>
2021-03-11 16:22:02 +0100

    Fix if & else

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,21 +10,26 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list aprm;
 	unsigned int i;
+	const char * varg;
 
 	va_start(aprm, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(aprm, const char *));
+		varg = va_arg(aprm, const char *);
+
+		if (varg != NULL)
+		{
+			printf("%s", varg);
+		}
+		else
+		{
+			printf("(nil)");
+		}
 
 		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
-		}
-
-		if (aprm == NULL)
-		{
-			printf("(nil)");
 		}
 	}
 	printf("\n");
