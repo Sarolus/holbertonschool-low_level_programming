6ada3d3e2860ae71a3b93ebfb6298c6b9c897944
Sarolus <cactussius.primus@gmail.com>
2021-03-18 20:26:34 +0100

    Gotta go fast

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void start (void) __attribute (constructor))
+
+/**
+ * start - constructor function to print before main function.
+ */
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
