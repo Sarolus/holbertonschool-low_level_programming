ae5b14263914351529e654df14fe882ec034055d
Sarolus <cactussius.primus@gmail.com>
2021-03-18 17:15:59 +0100

    New node - Head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String
+ * Return: return length.
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Beginning of our list_t
+ * @str: string parameter of list_t struct.
+ * Return: return NULL if one of the parameters is NULL,
+ * otherwise the address of the new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *h;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	h = malloc(sizeof(list_t));
+
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+
+	h->str = strdup(str);
+	h->len = _strlen(str);
+	*head = h;
+
+	return (*head);
+}
