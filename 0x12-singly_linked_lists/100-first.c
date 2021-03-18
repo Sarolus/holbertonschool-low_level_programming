c33a076fd22807462d0fb07fb0618aa9576e91b0
Sarolus <cactussius.primus@gmail.com>
2021-03-18 20:32:17 +0100

    Faster

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
-void start (void) __attribute__ ((constructor));
+void start(void) __attribute__ ((constructor));
 
 /**
  * start - constructor function to print before main function.
  */
 void start(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char fast[] = "You're beat! and yet, you must allow,\n";
+	char tortoise[] = "I bore my house upon my back!\n";
+
+	printf("%s%s", fast, tortoise);
 }
