e5cd99fd0cb9151192df2436892d94abff9e0166
Sarolus <cactussius.primus@gmail.com>
2021-03-18 19:09:51 +0100

    New node - End

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String
+ * Return: return length.
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Beginning of our list_t
+ * @str: string parameter of list_t struct.
+ * Return: return NULL if one of the parameters is NULL,
+ * otherwise the address of the new element.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *i;
+
+	i = *head;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (i->next != NULL)
+	{
+		i = i->next;
+	}
+	i->next = new;
+
+	return (i);
+}
