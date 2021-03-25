4dacbf777a7eea2e1aabab73bac099b2b0e9d3ec
Sarolus <cactussius.primus@gmail.com>
2021-03-25 18:54:52 +0100

    101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * count - function to count (OBVIOUSLY)
+ * @n: number
+ * Return: return the count.
+ */
+int count(unsigned long int n)
+{
+	if (n <= 0)
+	{
+		return (0);
+	}
+	if ((n & 1) == 1)
+	{
+		return (count((n >> 1)) + 1);
+	}
+	else
+	{
+		return (count((n >> 1)));
+	}
+}
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * to get from one number to another.
+ * @n: number
+ * @m: comparative number
+ * Return: return -1 if error, otherwise the number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int c;
+
+	c = n ^ m;
+
+	return (count(c));
+}
