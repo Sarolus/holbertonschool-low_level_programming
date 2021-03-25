4b6d1f13313a73ee4d2e17c4a56dc014441a0155
Sarolus <cactussius.primus@gmail.com>
2021-03-25 16:43:28 +0100

    1.1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * print_binary_recursiv - recursive function for the n > 0 conversion
+ * @n: number
+ */
+void print_binary_recursiv(unsigned long int n)
+{
+	if (n != 0)
+	{
+
+		print_binary_recursiv((n >> 1));
+
+		if ((n & 1) == 1)
+		{
+			_putchar('1');
+		}
+
+		if ((n & 1) == 0)
+		{
+			_putchar('0');
+		}
+	}
+}
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+	        print_binary_recursiv(n);
+	}
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,9 @@
 #define HOLBERTON_H
 #include <stddef.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+void print_binary_recursiv(unsigned long int n);
 
 #endif /* HOLBERTON_H */
