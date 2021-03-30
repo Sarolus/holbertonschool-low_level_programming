a104b9299373cfe8d33c3d901305ae72c50ac059
Sarolus <cactussius.primus@gmail.com>
2021-03-30 15:20:42 +0200

    Fix O_RDONLY

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
