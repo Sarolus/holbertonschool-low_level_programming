6fddf7441d37f2745d55323b73f86768cefc1e38
Sarolus <cactussius.primus@gmail.com>
2021-03-30 15:46:55 +0200

    1.Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * create_file - Creates a file.
+ *
+ * @filename: the file pointer.
+ * @text_content: the pointer of a null terminated string to write.
+ *
+ * Return: On success return 1, otherwise -1 if it's a failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	ssize_t len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	while (text_content && text_content[len])
+	{
+		len++;
+	}
+
+	wr = write(fd, text_content, len);
+
+	if (wr == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
