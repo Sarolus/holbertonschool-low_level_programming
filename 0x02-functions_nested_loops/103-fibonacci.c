a642fae4acfc70d70b87b2f047501b66e6b4e7ba
Sarolus <cactussius.primus@gmail.com>
2021-04-22 14:42:41 +0200

    Fibonacci numbers even-vaued

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the even-valued terms that do not exceed 4.000.000
+ * Return: return 0 on success.
+ */
+int main(void)
+{
+	int i = 0;
+	long int res, even, n1 = 1, n2 = 2;
+
+	while (i < 31)
+	{
+		if (i == 1)
+		{
+			if (n1 % 2 == 0)
+			{
+				even += n1;
+				printf("%ld", even);
+			}
+		}
+		else
+		{
+			if (n1 % 2 == 0)
+			{
+				even += n1;
+				printf(", %ld", even);
+			}
+		}
+		res = n1 + n2;
+		n1 = n2;
+		n2 = res;
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
