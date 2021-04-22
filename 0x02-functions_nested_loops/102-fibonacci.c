f554dabe6be6680fd7483770980027ac1fdc1830
Sarolus <cactussius.primus@gmail.com>
2021-04-22 14:32:23 +0200

    Fibonacci numbers 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers
+ * Return: return 0 on success.
+ */
+int main(void)
+{
+	int i = 0;
+	long int res, n1 = 1, n2 = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%ld", n1);
+		}
+		else
+		{
+			printf(", %ld", n1);
+		}
+		res = n1 + n2;
+		n1 = n2;
+		n2 = res;
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
