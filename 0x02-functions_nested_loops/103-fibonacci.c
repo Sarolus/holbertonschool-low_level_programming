5f6844853c26fef69618d6d04e1cd414fcfbb526
Sarolus <cactussius.primus@gmail.com>
2021-04-22 14:45:30 +0200

    Fibonacci numbers even-valued

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,24 +10,14 @@ int main(void)
 	int i = 0;
 	long int res, even, n1 = 1, n2 = 2;
 
-	while (i < 33)
+	while (i < 31)
 	{
-		if (i == 1)
+		if (n1 % 2 == 0)
 		{
-			if (n1 % 2 == 0)
-			{
-				even += n1;
-				printf("%ld", even);
-			}
+			even += n1;
+			printf("%ld", even);
 		}
-		else
-		{
-			if (n1 % 2 == 0)
-			{
-				even += n1;
-				printf(", %ld", even);
-			}
-		}
+
 		res = n1 + n2;
 		n1 = n2;
 		n2 = res;
